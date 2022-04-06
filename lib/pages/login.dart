27e49d69d8b16c03b4e5461ea5f3cfbd83566f02
miguelopezv <miguelopezv@gmail.com>
2022-04-05 20:49:49 -0500

    use card container on login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,6 +6,25 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: AuthBg());
+    return Scaffold(
+        body: AuthBg(
+            child: SingleChildScrollView(
+      child: Column(children: [
+        const SizedBox(
+          height: 250,
+        ),
+        CardContainer(
+          child: Column(children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Text('Login', style: Theme.of(context).textTheme.headline4),
+            const SizedBox(
+              height: 30,
+            )
+          ]),
+        )
+      ]),
+    )));
   }
 }
