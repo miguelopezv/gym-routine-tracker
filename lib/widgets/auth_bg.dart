37bf95f1c84273a7e0b4d25ca7a22c1c0abf4843
miguelopezv <miguelopezv@gmail.com>
2022-04-05 20:06:57 -0500

    update bg widget

--- lib/widgets/auth_bg.dart
+++ lib/widgets/auth_bg.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
 class AuthBg extends StatelessWidget {
-  const AuthBg({Key? key}) : super(key: key);
+  final Widget child;
+
+  const AuthBg({Key? key, required this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: Stack(children: [_ColorBox()]),
+    return SizedBox(
+      child: Stack(children: [_ColorBox(), _LoginIcon(), child]),
+    );
+  }
+}
+
+class _LoginIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.only(top: 30),
+        child: const Icon(
+          Icons.person_pin,
+          color: Colors.white,
+          size: 100,
+        ),
+      ),
     );
   }
 }
