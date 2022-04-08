b678d36daee31641623efb16146b2141910801d8
miguelopezv <miguelopezv@gmail.com>
2022-04-07 21:31:21 -0500

    added email regex

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -1,3 +1,6 @@
+String emailPattern =
+    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
 extension StringExtension on String {
   String capitalize() {
     return "${this[0].toUpperCase()}${substring(1).toLowerCase()}";
