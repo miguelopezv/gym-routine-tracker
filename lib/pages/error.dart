d79b3a935ac62be97fa15b4148daba6eff187013
Miguel López <miguelopezv@gmail.com>
2022-04-15 18:11:41 -0500

    create loading and error pages

---
+++ lib/pages/error.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ErrorScreen extends StatelessWidget {
+  const ErrorScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: show error message
+    return const Text("Oops, something went wrong 😔");
+  }
+}
--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,2 +1,4 @@
-export 'package:gym_routine/pages/home.dart';
 export 'package:gym_routine/pages/login.dart';
+export 'package:gym_routine/pages/home.dart';
+export 'package:gym_routine/pages/loading.dart';
+export 'package:gym_routine/pages/error.dart';
