2cb587b39cbe91f00ce3dbd804190ee07efd9c32
Miguel López <miguelopezv@gmail.com>
2022-04-16 15:21:31 -0500

    Home page with sign out

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gym_routine/providers/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Consumer(builder: ((context, ref, child) {
+      final _auth = ref.watch(authenticationProvider);
+      return Scaffold(
         body: Center(
-      child: Text("Home"),
-    ));
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("Home"),
+              MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  disabledColor: Colors.grey,
+                  color: Colors.blueAccent,
+                  child: const Text(
+                    'sign Out',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: (() => _auth.signOut())),
+            ],
+          ),
+        ),
+      );
+    }));
   }
 }
