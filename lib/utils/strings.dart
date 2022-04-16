f766caac37f939f23824d74c6d24aac732f2cf71
Miguel López <miguelopezv@gmail.com>
2022-04-16 15:20:00 -0500

    add extension to validate email

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -1,12 +1,18 @@
 String emailPattern =
     r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
 
-extension StringExtension on String {
+extension StringCapitalize on String {
   String capitalize() {
     return "${this[0].toUpperCase()}${substring(1).toLowerCase()}";
   }
 }
 
-String formatFirestoreError(string) {
+extension MailValidation on String {
+  bool isValidEmail() {
+    return RegExp(emailPattern).hasMatch(this);
+  }
+}
+
+String formatFirestoreError(String string) {
   return string.replaceAll('-', ' ').capitalize();
 }
